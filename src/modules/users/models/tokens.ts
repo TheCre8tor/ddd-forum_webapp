e85caafb86ddfa362088b966665b4c5a4ac148a7
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-18 11:39:41 +0100

    Added user models

---
+++ src/modules/users/models/tokens.ts
@@ -0,0 +1,11 @@
+export interface JWTTokenClaims {
+    userId: string;
+    isEmailVerified: boolean;
+    email: string;
+    adminUser: boolean;
+    sessionId?: string;
+}
+
+export type JWTToken = string;
+
+export type RefreshToken = string;
