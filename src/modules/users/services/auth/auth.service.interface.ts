55f022baf59a2c6fcaaf698ee4103e1c0e748c40
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-25 12:10:30 +0100

    Updated the auth service interface

--- src/modules/users/services/auth/auth.service.interface.ts
+++ src/modules/users/services/auth/auth.service.interface.ts
@@ -4,7 +4,9 @@ export type TokenType = 'access-token' | 'refresh-token';
 
 export interface IAuthService {
     isAuthenticated(): boolean;
-    getToken(tokenType: TokenType): JWTToken | RefreshToken;
+    encryptToken(token: string): string;
+    decryptToken(token: string): string | null;
+    getToken(tokenType: TokenType): JWTToken | RefreshToken | null;
     setToken(tokenType: TokenType, token: JWTToken | RefreshToken): void;
     removeToken(tokenType: TokenType): void;
 }
