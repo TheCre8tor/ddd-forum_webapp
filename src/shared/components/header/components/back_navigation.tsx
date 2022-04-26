a97617aa67ce97a4d632a5cc143ffc49911ad88d
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-04-26 13:09:31 +0100

    Completed the BackNavigation Button

--- src/shared/components/header/components/back_navigation.tsx
+++ src/shared/components/header/components/back_navigation.tsx
@@ -1,1 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+interface BackNavigationProps {
+    to: string;
+    text: string;
+}
+
+const BackNavigation: FC<BackNavigationProps> = props => {
+    return (
+        <Link to={props.to} className="back-nav">
+            <p>{props.text}</p>
+        </Link>
+    );
+};
+
+export default BackNavigation;
